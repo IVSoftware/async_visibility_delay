d5af2e203d045c7681bca1a273e16aaef28326e8
Tom Gregor <tgregor@ivsoftware.com>
2022-05-26 09:52:41 -0600

    Add project files.

---
+++ async_visibility_delay/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace async_visibility_delay
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            makeVisibleOnDelay();
+        }
+
+        private async void makeVisibleOnDelay()
+        {
+            await Task.Delay(1000);
+            panel1.Visible = true;
+            await Task.Delay(1000);
+            panel2.Visible = true;
+        }
+    }
+}
